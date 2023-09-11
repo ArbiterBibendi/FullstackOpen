524e93cacf2d559946d97e4d0f7b780c0726e3a9
ArbiterBibendi <ArbiterBibendi@yahoo.com>
2023-09-11 18:14:02 -0500

    3.1-3.6

---
+++ part3/phonebookbackend/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+app.use(express.json())
+
+let phonebook = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+app.get('/api/persons', (request, response) => {
+    response.json(phonebook);
+});
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = phonebook.find((person) => person.id === id);
+    if (!person) {
+        return response.status(404).end()
+    }
+    response.json(person);
+});
+app.get('/info', (request, response) => {
+    response.send(
+        `<p>Phonebook has info for ${phonebook.length} people</p>` +
+        `<p>${Date()}</p>`
+    );
+});
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    phonebook = phonebook.filter((person) => person.id !== id);
+    response.status(202);
+    response.end();
+});
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+    if (!body.name) {
+        response.status(400);
+        return response.json({ error: 'name is missing' });
+    }
+    else if (!body.number) {
+        response.status(400);
+        return response.json({ error: 'number is missing' });
+    }
+    else if (phonebook.find((person) => person.name.toLowerCase() == body.name.toLowerCase())) {
+        response.status(400);
+        return response.json({ error: 'name must be unique' });
+    }
+    const person = {
+        name: body.name,
+        id: Math.floor(Math.random() * 999999999999999),
+        number: body.number
+    }
+    phonebook = phonebook.concat(person);
+    response.send(person);
+});
+
+
+app.listen(3001, () => {
+    console.log('Listening');
+});
