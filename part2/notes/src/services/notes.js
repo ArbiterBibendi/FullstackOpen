e8591a7006a79ad5513967d53941283b872748f3
ArbiterBibendi <ArbiterBibendi@yahoo.com>
2023-09-11 20:10:48 -0500

    connect notes to backend

--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-const baseurl = 'http://localhost:3001/notes'
+const baseUrl = 'http://localhost:3001/api/notes'
 
 const getAll = () => {
-    const request = axios.get(baseurl);
+    const request = axios.get(baseUrl);
     const nonExisting = {
         id: 10000,
         content: 'This note is not saved to server',
@@ -17,7 +17,7 @@ const create = (newObject) => {
 }
 
 const update = (id, newObject) => {
-    const request = axios.put(`${baseurl}/${id}`, newObject);
+    const request = axios.put(`${baseUrl}/${id}`, newObject);
     return request.then(response => response.data);
 }
 
--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express()
+app.use(cors());
 app.use(express.json())
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method);
@@ -76,7 +78,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
