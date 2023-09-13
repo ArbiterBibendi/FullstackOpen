4e9de2cbf9f16a68876fd5244b77574e82e4d692
ArbiterBibendi <ArbiterBibendi@yahoo.com>
2023-09-12 21:18:28 -0500

    oopers

--- part3/notes/src/App.jsx
+++ part3/notes/src/App.jsx
@@ -26,8 +26,7 @@ const App = () => {
     e.preventDefault();
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
-      id: notes.length + 1
+      important: Math.random() < 0.5
     };
 
     noteService
