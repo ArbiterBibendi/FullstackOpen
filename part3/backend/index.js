6d789d564b2d3b30e518f161c3962db719e55377
ArbiterBibendi <ArbiterBibendi@yahoo.com>
2023-09-12 23:10:38 -0500

    integrated notes backend with mongodb

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -18,6 +18,7 @@ app.use(requestLogger);
 
 
 
+
 let notes = [
     {
         id: 1,
@@ -45,21 +46,27 @@ app.get('/api/notes', (request, response) => {
         response.json(notes);
     });
 });
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note);
-    });
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note);
+            }
+            else {
+                response.status(404).end();
+            }
+        })
+        .catch((e) => {
+            next(e);
+        });
 });
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter((note) => note.id !== id);
-
-    response.status(204).end();
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then((result => {
+            return response.status(204).end();
+        }))
+        .catch(e => next(e))
 });
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
-    return maxId + 1;
-}
 app.post('/api/notes', (request, response) => {
     const body = request.body;
     if (!body.content) {
@@ -74,12 +81,31 @@ app.post('/api/notes', (request, response) => {
         response.json(note);
     });
 });
+app.put('/api/notes/:id', (request, response, next) => {
+    const note = {
+        content: request.body.content,
+        important: request.body.important
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(result => response.json(result))
+        .catch(e => next(e));
+});
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' });
 }
-
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
+    }
+    next(error);
+}
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
