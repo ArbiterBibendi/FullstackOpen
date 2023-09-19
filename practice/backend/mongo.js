f45db0c98398d194390be3e79405afdfa5211f15
ArbiterBibendi <ArbiterBibendi@yahoo.com>
2023-09-19 17:17:48 -0500

    but fr this time

---
+++ practice/backend/mongo.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+	console.log('give password as argument');
+	process.exit();
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://bibendi:${password}@cluster0.nf3xyqx.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+	content: String,
+	important: Boolean
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+Note.find({}).then(result => {
+	result.forEach(note => console.log(note));
+	mongoose.connection.close();
+});
