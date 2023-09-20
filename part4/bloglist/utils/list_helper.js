b75fa689f00a7b197d0a18b707caaa936aa7d4d3
ArbiterBibendi <ArbiterBibendi@yahoo.com>
2023-09-19 19:37:31 -0500

    4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,7 +10,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const mostLiked = blogs.reduce((acc, blog, index) => {
+    return acc?.likes > blog?.likes ? acc : blog;
+  }, null);
+  return mostLiked;
+};
+favoriteBlog([{likes: 2}, {likes: 5}]);
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
