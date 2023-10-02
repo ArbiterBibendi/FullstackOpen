9bf9cc3b3cb6bdad69cbbb4a887cd1e12beb87f0
ArbiterBibendi <ArbiterBibendi@yahoo.com>
2023-10-02 18:26:37 -0500

    5.1*

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -14,11 +14,12 @@ const App = () => {
   }, [])
 
   if (!user) {
-    return <LoginForm setUser={setUser} />
+    return <LoginForm setUser={setUser} />;
   }
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} is logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- part5/bloglist-frontend/src/components/LoginForm.jsx
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -6,20 +6,27 @@ const LoginForm = ({ setUser }) => {
     const [password, setPassword] = useState('');
     const handleLogin = async (e) => {
         e.preventDefault();
-        try{
-            const user = await loginService.login({username, password});
+        try {
+            const user = await loginService.login({ username, password });
             setUser(user);
             console.log(user);
-        } catch(e) {
+        } catch (e) {
             console.log(e);
         }
     }
     return (
         <div>
+            <h1>log in to application</h1>
             <form onSubmit={handleLogin}>
-                Username <input type='text' name='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
-                Password <input type='text' name='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type='submit'>Login</button>
+                <div>
+                    username<input type='text' name='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+                </div>
+                <div>
+                    password<input type='text' name='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <div>
+                    <button type='submit'>Login</button>
+                </div>
             </form>
         </div>
     );
