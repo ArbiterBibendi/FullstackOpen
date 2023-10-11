addbecc8af990e61f4a996f1b3e66cd387f5a5eb
ArbiterBibendi <ArbiterBibendi@yahoo.com>
2023-10-11 15:52:54 -0500

    5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
     setUser(null);
   }
   const handleLike = async (blog) => {
-    const newBlog = {...blog, likes: blog.likes + 1}
+    const newBlog = { ...blog, likes: blog.likes + 1 }
     const response = await blogService.likeBlog(newBlog);
     setBlogs(blogs.map((blog) => blog.id !== newBlog.id ? blog : newBlog));
   }
@@ -47,6 +47,17 @@ const App = () => {
     setTimeout(() => {
       setNotification({ content: null });
     }, 5000);
+  }
+
+  const compareFn = (a, b) => {
+    return b.likes - a.likes;
+  }
+  const getSortedBlogComponents = () => {
+    const sortedBlogs = blogs.sort(compareFn);
+    const sortedBlogComponents = sortedBlogs.map(blog =>
+      <Blog key={blog.id} blog={blog} handleLike={handleLike} />
+    );
+    return sortedBlogComponents;
   }
 
   const loggedIn = (
@@ -69,9 +80,7 @@ const App = () => {
       <h2>blogs</h2>
       {user ? loggedIn : loggedOut}
 
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} handleLike={handleLike}/>
-      )}
+      {getSortedBlogComponents()}
     </div>
   )
 }
